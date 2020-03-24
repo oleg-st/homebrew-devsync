e7964657d76b2b5f592cb657e66498a199ca5525
Oleg Stepanischev <Oleg.Stepanischev@tatar.ru>
2020-03-24 18:42:10 +0300

    Initial

---
+++ devsync.rb
@@ -0,0 +1,32 @@
+require "formula"
+
+class DotnetRequirement < Requirement
+  fatal true
+
+  satisfy(:build_env => false) { which("dotnet") }
+
+  def message; <<~EOS
+    .NET Core 3+ Runtime is required; install it via:
+    brew cask install dotnet
+    EOS
+  end
+end
+
+class Devsync < Formula
+  depends_on DotnetRequirement
+  version "0.20"
+  url "https://github.com/oleg-st/DevSync/releases/download/#{version}/DevSync.zip"
+  homepage "https://github.com/oleg-st/DevSync"
+  sha256 "9cea7648af3229035273c715d9ea766dea25516f233898f5c734b2ee27933882"
+
+  def install
+    # write run script
+    dotnet = which("dotnet")
+    (bin/"DevSync").write <<~EOS
+    #!/bin/bash
+    exec "#{dotnet}" "#{prefix}/DevSync.dll" "$@"
+    EOS
+    # install all files
+    prefix.install Dir["*"]
+  end
+end
