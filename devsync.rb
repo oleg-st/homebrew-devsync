586b720d6f5faba11770468fa05e1884bb557f96
Oleg Stepanischev <Oleg.Stepanischev@tatar.ru>
2020-07-03 00:49:20 +0300

    0.30

--- devsync.rb
+++ devsync.rb
@@ -14,10 +14,10 @@ end
 
 class Devsync < Formula
   depends_on DotnetRequirement
-  version "0.29"
+  version "0.30"
   url "https://github.com/oleg-st/DevSync/releases/download/#{version}/DevSync.zip"
   homepage "https://github.com/oleg-st/DevSync"
-  sha256 "a35347a41eb6eede72b9c7ba8741ef4db10a51f932b9ab051c86ec6c3a28254c"
+  sha256 "dc917c9a1bd5d4dd27f66b9333fbce4e9af4cdead56e3fa4c4368cdac3e0600f"
 
   def install
     # write run script
